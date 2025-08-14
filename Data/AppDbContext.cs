306ce7b625df62547a876cb7faa7cb204e8f96bb
Daylen Smith <daylencsmith@gmail.com>
2025-08-13 19:39:22 -0700

    configure basic models

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DensityReportingToolBackend.Models;
+
+namespace DensityReportingToolBackend.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GeoPacificEmployee> GeoPacificEmployees { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<GeoPacificEmployee>()
+                .HasOne(e => e.Role)
+                .WithMany(r => r.Employees)
+                .HasForeignKey(e => e.RoleId)
+                .IsRequired();
+        }
+    }
+}
---
+++ Models/PersonalInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace DensityReportingToolBackend.Models
+{
+    public class PersonalInfo
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+    }
+
+    public class GeoPacificEmployee: PersonalInfo
+    {
+        public int RoleId { get; set; }
+        public Role Role { get; set; } = null!;
+    }
+
+    public class Role
+    {
+        public int Id { get; set; }
+        public required string RoleTitle { get; set; }
+
+        public ICollection<GeoPacificEmployee> Employees { get; set; } = [];
+    }
+}
