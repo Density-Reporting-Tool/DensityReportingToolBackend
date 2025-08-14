5474c7e10cd27d37b6e246e191279dd1c2c578c9
Daylen Smith <daylencsmith@gmail.com>
2025-08-13 19:13:27 -0700

    Add comments for understanding how this all works

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
+//Creates a WebApplicationBuilder which sets up configuration, logging, dependency injection, etc.
+//args comes from the command line and can be used for custom configuration.
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//Registers MVC controllers so your API endpoints can respond to requests.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Enables API metadata for minimal APIs, used by Swagger.
 builder.Services.AddEndpointsApiExplorer();
+// Adds Swagger/OpenAPI generation, so you can view and test your API in /swagger
 builder.Services.AddSwaggerGen();
 
+// Returns a WebApplication instance, which represents your running server
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,11 +20,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+// Redirects HTTP requests to HTTPS automatically
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+// Maps your controllers so they handle incoming requests
 app.MapControllers();
 
+// Starts the server
 app.Run();
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # DensityReportingTooltBackend
+
+## Getting started Notes
+appsettings.json: Base config for the app, has settings that apply to ALL environments
+appsettings.development.json: Only loaded when environment is set to development, overrides values in appsettings.json
+appsettings.development.json is ignored, use for Db connection strings, API keys, secrets
+
+Properties/launchSettings.json: controls how the app is run during development, defines environment variables
+                                for each profile, not used in production
+Properties/PublishProfiles/*: Contains .pubxml files which determine where you're deploying your app and the configuration
+
+DensityReportingToolBackend.http: lets us run HTTP requests difrectly from VisualStudio without needing Postman, curl, etc. OPTIONAL
