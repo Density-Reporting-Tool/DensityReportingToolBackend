a6a0ad4c6a6d7fcc593a11496104bc0f058e162f
Peter <Peter.Senyk@gmail.com>
2025-09-17 12:43:20 -0700

    feat: add iproctorservice as service interface

---
+++ Services/IProctorService.cs
@@ -0,0 +1,17 @@
+using DensityReportingToolBackend.Models.DTOs;
+
+namespace DensityReportingToolBackend.Services
+{
+    public interface IProctorService
+    {
+        /// <summary>
+        /// Create a new proctor (Lab Admin functionality)
+        /// </summary>
+        Task<ProctorCreateResponse> CreateProctorAsync(CreateProctorRequest request);
+        
+        // TODO: Add other methods as needed
+        // Task<ProctorUpdateResponse?> UpdateProctorAsync(int id, UpdateProctorRequest request);
+        // Task<ProctorDataResponse?> GetProctorAsync(int id);
+        // Task<IEnumerable<ProctorDataResponse>> GetProctorsForJobAsync(string jobNumber);
+    }
+}
