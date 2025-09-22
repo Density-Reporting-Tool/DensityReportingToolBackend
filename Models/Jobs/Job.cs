efc0b2329c579a12e0417e4c0a429da321b2a840
Daylen Smith <daylencsmith@gmail.com>
2025-09-21 17:56:28 -0700

    refacotr job sytem import

--- Models/Jobs/Job.cs
+++ Models/Jobs/Job.cs
@@ -1,15 +1,17 @@
 namespace DensityReportingToolBackend.Models;
 
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Job
 {
     public int Id { get; set; }
-    
+
     // Job number - can be numeric (e.g., "25482") or alphanumeric (e.g., "15827-A")
     public required string JobNumber { get; set; }
-    
+
     // Client name as a simple string
     public required string ClientName { get; set; }
-    
+
     public required string ProjectName { get; set; }
     public required string SiteAddress { get; set; }
 
@@ -27,42 +29,42 @@ public class Job
     public ICollection<JobSiteContact> SiteContacts { get; set; } = [];
 
     // Computed properties for easy access to project managers
-    [System.ComponentModel.DataAnnotations.Schema.NotMapped]
-    public ICollection<PersonalInfo> ActiveProjectManagers => 
+    [NotMapped]
+    public ICollection<PersonalInfo> ActiveProjectManagers =>
         ProjectManagers?
             .Where(pm => pm.IsActive && pm.EndDate == null)
             .Select(pm => pm.PersonalInfo)
             .ToList() ?? [];
 
-    [System.ComponentModel.DataAnnotations.Schema.NotMapped]
-    public PersonalInfo? ActiveProjectManager => 
+    [NotMapped]
+    public PersonalInfo? ActiveProjectManager =>
         ActiveProjectManagers.FirstOrDefault();
 
-    [System.ComponentModel.DataAnnotations.Schema.NotMapped]
-    public ICollection<PersonalInfo> AllProjectManagers => 
+    [NotMapped]
+    public ICollection<PersonalInfo> AllProjectManagers =>
         ProjectManagers?
             .Select(pm => pm.PersonalInfo)
             .ToList() ?? [];
 
     // Computed properties for easy access to site contacts
-    [System.ComponentModel.DataAnnotations.Schema.NotMapped]
-    public ICollection<PersonalInfo> ActiveSiteContact => 
+    [NotMapped]
+    public ICollection<PersonalInfo> ActiveSiteContact =>
         SiteContacts?
             .Where(sc => sc.IsActive)
             .Select(sc => sc.PersonalInfo)
             .ToList() ?? [];
 
-    [System.ComponentModel.DataAnnotations.Schema.NotMapped]
-    public ICollection<PersonalInfo> AllSiteContacts => 
+    [NotMapped]
+    public ICollection<PersonalInfo> AllSiteContacts =>
         SiteContacts?
             .Select(sc => sc.PersonalInfo)
             .ToList() ?? [];
 
     // Proctors that belong directly to this job (through LabTest)
-    [System.ComponentModel.DataAnnotations.Schema.NotMapped]
+    [NotMapped]
     public ICollection<Proctor> DirectProctors => LabTests?.SelectMany(lt => lt.Proctors).ToList() ?? [];
 
     // Proctors from other jobs that are being reused for this job
-    [System.ComponentModel.DataAnnotations.Schema.NotMapped]
+    [NotMapped]
     public ICollection<Proctor> ReusedProctors => ProctorAdditionalJobs?.Select(paj => paj.Proctor).ToList() ?? [];
 }
