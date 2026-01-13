2b410186ac49f15423a3d557401a1c97c41a6cb3
Daylen Smith <daylencsmith@gmail.com>
2026-01-12 16:20:23 -0800

    fix job dto mappings

--- Mappings/Jobs/Job.cs
+++ Mappings/Jobs/Job.cs
@@ -3,21 +3,51 @@ namespace DensityReportingToolBackend.Mappings.Jobs;
 using AutoMapper;
 using DensityReportingToolBackend.Models;
 using DensityReportingToolBackend.DTOs.Jobs;
+using DensityReportingToolBackend.DTOs.People;
+using DensityReportingToolBackend.DTOs.Density;
+using DensityReportingToolBackend.DTOs.Reports;
+using DensityReportingToolBackend.DTOs.Labs;
 
 public class JobMappingProfile : Profile
 {
     public JobMappingProfile()
     {
-        // Entity -> DTO (For Reading)
+        // 1. Core Job Mappings
         CreateMap<Job, JobReadDto>();
-
-        // DTO -> Entity (For Creating)
-        // We ignore the Id property because the Database will auto-generate it
-        CreateMap<JobCreateDto, Job>()
-            .ForMember(dest => dest.Id, opt => opt.Ignore());
-
-        // DTO -> Entity (For Updating)
-        // This maps the DTO values onto an existing Entity from the DB
+        CreateMap<JobCreateDto, Job>().ForMember(dest => dest.Id, opt => opt.Ignore());
         CreateMap<JobUpdateDto, Job>();
+
+        // 2. Job Sub-Collections
+        CreateMap<JobProjectManager, JobProjectManagerReadDto>();
+        CreateMap<JobSiteContact, JobSiteContactReadDto>();
+        CreateMap<JobNote, JobNoteReadDto>();
+        CreateMap<DistributionList, DistributionListReadDto>();
+        CreateMap<DistributionMember, DistributionMemberReadDto>();
+        CreateMap<SitePlan, SitePlanReadDto>();
+
+        // 3. People / PersonalInfo (Required for ProjectManagers and SiteContacts)
+        CreateMap<PersonalInfo, PersonalInfoReadDto>();
+        CreateMap<GeoPacificEmployee, GeoPacificEmployeeReadDto>();
+        CreateMap<Role, RoleReadDto>();
+
+        // 4. Reports and Memos
+        CreateMap<Report, ReportReadDto>();
+        CreateMap<ReportPhoto, ReportPhotoReadDto>();
+        CreateMap<ReportMemo, ReportMemoReadDto>();
+        CreateMap<MemoComment, MemoCommentReadDto>();
+
+        // 5. Density and Labs
+        CreateMap<DensityTest, DensityTestReadDto>();
+        CreateMap<DensityTestComment, DensityTestCommentReadDto>();
+        CreateMap<ShotPlacement, ShotPlacementReadDto>();
+        
+        // Use the custom logic for Sieve computed properties
+        CreateMap<Sieve, SieveReadDto>()
+            .ForMember(dest => dest.ComputedRows, opt => opt.MapFrom(src => src.ComputePercentages()));
+        
+        CreateMap<SieveResult, SieveResultReadDto>();
+        CreateMap<Proctor, ProctorReadDto>();
+        CreateMap<ProctorType, ProctorTypeReadDto>();
+        CreateMap<ProctorAdditionalJob, ProctorAdditionalJobReadDto>();
     }
 }
--- Models/People/PersonalInfo.cs
+++ Models/People/PersonalInfo.cs
@@ -1,6 +1,6 @@
 namespace DensityReportingToolBackend.Models;
 
-public class PersonalInfo: ModelBaseWithDto<PersonalInfo, PersonalInfoReadDto>
+public class PersonalInfo: ModelBase
 {
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
@@ -12,32 +12,4 @@ public class PersonalInfo: ModelBaseWithDto<PersonalInfo, PersonalInfoReadDto>
 
     // Navigation properties to distinguish between employee and contractor
     public GeoPacificEmployee? Employee { get; set; }
-}
-
-public class PersonalInfoBaseDto
-{
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
-    public string? Company { get; set; }
-}
-
-public class PersonalInfoCreateDto : PersonalInfoBaseDto { }
-
-public class PersonalInfoUpdateDto : PersonalInfoBaseDto { }
-
-public class PersonalInfoReadDto : PersonalInfoBaseDto
-{
-    public int Id { get; set; }
-
-    public PersonalInfoReadDto(PersonalInfo info, HashSet<(Type, int)> visited)
-    {
-        Id = info.Id;
-        FirstName = info.FirstName;
-        LastName = info.LastName;
-        Email = info.Email;
-        PhoneNumber = info.PhoneNumber;
-        Company = info.Company;
-    }
 }
